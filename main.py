e8f3b6f3467854a6e0f125af6261414571b1ff58
Jared Rabie <jared.rabie@gmail.com>
2022-04-02 16:42:47 +1300

    Now prompts user to input directory

--- main.py
+++ main.py
@@ -3,6 +3,21 @@ import os
 root_directory = "C:/Users/Jared Rabie/Root"
 print(os.listdir(root_directory))
 
+def get_root_directory(invalid = 0):
+    if invalid == 0:
+        dir = input("Please input a root directory: ")
+    else:
+        dir = input("Invalid directory! Try again: ")
+
+    try:
+        os.listdir(root_directory)
+    except NotADirectoryError:
+        get_root_directory(1)
+    except:
+        print("Unknown Error")
+    
+    return dir
+
 def explore_folder(path, depth = 0):
     try:
         lst = os.listdir(path)
@@ -15,4 +30,5 @@ def explore_folder(path, depth = 0):
             explore_folder(path + "/{}/".format(item), depth+1)
 
 
+root_directory = get_root_directory()
 explore_folder(root_directory)
