b7532b82b92aa9e4f3d6bb33a0eef4fba8454c1e
Jared Rabie <jared.rabie@gmail.com>
2022-04-02 22:54:14 +1300

    Added ability to save map to file

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import os
 
-root_directory = "C:/Users/Jared Rabie/Root"
-print(os.listdir(root_directory))
+def get_save_name(invalid = 0):
+    if invalid == 0:
+        save_name = input("Please enter save file name (Leave blank to prevent saving): ")
+    else:
+        save_name = input("Invalid directory! Try again: ")
+    
+    if save_name == "":
+        return None
+    try:
+        save_file = open(save_name, "w")
+        save_file.close()
+    except:
+        print("Error! Please try again.")
+        get_save_name(1)
+    
+    return save_name
 
 def get_root_directory(invalid = 0):
     if invalid == 0:
@@ -15,20 +29,29 @@ def get_root_directory(invalid = 0):
         get_root_directory(1)
     except:
         print("Unknown Error")
-    
     return dir
 
-def explore_folder(path, depth = 0):
+def explore_folder(root_directory, depth = 0):
+    
     try:
-        lst = os.listdir(path)
+        lst = os.listdir(root_directory)
     except NotADirectoryError:
         return ...
     lst.sort()
+
     for item in lst:
+        if save_name != None:
+            save_file.write("\t"*depth+item+"\n")
         print("\t"*depth, item, sep="")
         if "." not in item:
-            explore_folder(path + "/{}/".format(item), depth+1)
+            explore_folder(root_directory + "/{}/".format(item), depth+1)
 
 
+
+save_name = get_save_name()
+if save_name != None:
+    save_file = open(save_name, 'w')
+    
 root_directory = get_root_directory()
 explore_folder(root_directory)
+save_file.close()
